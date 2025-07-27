b456d6c0da14786951be0346138044fc7845f010
Victor Emeka <98593069+victoremeka@users.noreply.github.com>
2025-07-27 22:47:43 +0100

    working qloo api implementation for sustainability metrics

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -1,12 +1,14 @@
-import requests
-
+import requests, os
 from google import genai
 from urllib import parse
 from enum import Enum
+from dotenv import load_dotenv
+from urllib import parse
+load_dotenv()
 
-
-
-QLOO_API_URL = "https://hackathon.api.qloo.com/v2"
+# QLOO_API_URL = "https://hackathon.api.qloo.com/"
+# Insights -> "/v2/insights"
+# Search -> "/search?query="
 
 class EntityType(Enum):
     artist = "urn:entity:artist"
@@ -20,24 +22,44 @@ class EntityType(Enum):
     tv_show = "urn:entity:tv_show"
     video_game = "urn:entity:video_game"
 
+class QlooData(Enum):
+    api_url = "https://hackathon.api.qloo.com"
+    headers = {
+        "accept": "application/json",
+        "X-Api-Key": os.getenv("QLOO_API_KEY")
+    }
+
+def entity_search(query:str, page_index=1):
+    url = f"{QlooData.api_url}/search?query={parse.quote(query)}&page={page_index}"
+    try:
+        response = requests.get(url, headers=QlooData.headers, timeout=10)
+    except requests.exceptions.RequestException as e:
+        print(f"Qloo API failed: {e}")
+        return
+    
+    data = response.json()["results"]
+
+    for entity in data:
+        if entity['types'][0] == 'urn:entity:destination':
+            return entity['entity_id']
+
+
+def get_entity_affinities(entity_id:str, filters:dict[str]):
+    url = f"{QlooData.api_url}/v2/insights?filter.type={filters["type"]}&filter.location={entity_id}&filter.tag.types={"urn:tag:sustainability_initiative:qloo"}"
+    try:
+        response = requests.get(url, headers=QlooData.headers, timeout=10)
+    except requests.exceptions.RequestException as e:
+        print(f"Qloo API failed: {e}")
+        return
+    
+    data = response.json()
+    return data["results"]['tags']
+
+        
 def generate_summary():
     client = genai.Client()
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents="" #TODO
     )
-    return response.text
-
-
-
-def generate_recommendation(config, query):
-    url = parse.quote(f"{QLOO_API_URL}/insights?filter.type={query}")
-
-    headers = {
-        "accept": "application/json",
-        "X-Api-Key": config
-        }
-
-    response = requests.get(url, headers=headers)
-
     return response.text
